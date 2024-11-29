45d3dc4409147f1ca2a03b9734da4cc0e8f80585
Alexandre Picavet <apicavet@hotmail.fr>
2024-11-29 14:18:26 +0100

    refactor(script): Split the getElement method and add JsDoc

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Google Cloud SDK Autoconnect
 // @description  Auto connect to your gcloud account when using 'gcloud auth login' command
-// @version      1.1.0
+// @version      1.2.0
 // @author       Alexandre Picavet (https://github.com/AlexandrePicavet)
 // @namespace    https://github.com/AlexandrePicavet/Google-Cloud-SDK-Autoconnect
 // @supportURL   https://github.com/AlexandrePicavet/Google-Cloud-SDK-Autoconnect/issues
@@ -15,62 +15,101 @@
 // @grant        window.close
 // ==/UserScript==
 
+// @ts-check
 (function() {
 	"use strict";
 
 	const application = "Google Cloud SDK";
 
-	function getElement(query, multi, predicate = () => true) {
-		const selectElement = () =>
-			multi ? document.querySelectorAll(query) : document.querySelector(query);
-		const existsAndMatches = (element) =>
-			element && predicate(multi ? [...element] : element);
+	/**
+	 * @param {string} query
+	 * @param {(element: HTMLElement) => boolean} [predicate=() => true]
+	 *
+	 * @returns {Promise<HTMLElement>}
+	 */
+	function getElement(query, predicate = () => true) {
+		return new Promise((resolve) =>
+			observe(() => {
+				/** @type HTMLElement | null */
+				const element = document.querySelector(query);
+				const elementMatches = element !== null && predicate(element);
 
-		return new Promise((resolve) => {
-			const element = selectElement();
-
-			if (existsAndMatches(element)) {
-				resolve(element);
-				return;
-			}
-
-			const observer = new MutationObserver(() => {
-				const observedElement = selectElement();
-
-				if (existsAndMatches(observedElement)) {
-					observer.disconnect();
-					resolve(observedElement);
+				if (elementMatches) {
+					resolve(element);
 				}
-			});
 
-			observer.observe(document.body, {
-				childList: true,
-				subtree: true,
-			});
+				return elementMatches;
+			}),
+		);
+	}
+
+	/**
+	 * @param {string} query
+	 * @param {(elements: HTMLElement[]) => boolean} [predicate=() => true]
+	 *
+	 * @returns {Promise<HTMLElement[]>}
+	 */
+	function getElements(query, predicate = () => true) {
+		return new Promise((resolve) =>
+			observe(() => {
+				const elements = /** @type {HTMLElement[]} */ ([
+					...document.querySelectorAll(query),
+				]);
+				const elementsMatches = elements.length > 0 && predicate(elements);
+
+				if (elementsMatches) {
+					resolve(elements);
+				}
+
+				return elementsMatches;
+			}),
+		);
+	}
+
+	/**
+	 * @param {() => boolean} onMutation that returns a boolean as to wether to disconnect from the observer or not.
+	 */
+	function observe(onMutation) {
+		const isAlreadyFound = onMutation();
+
+		if(isAlreadyFound) {
+			return;
+		}
+
+		const observer = new MutationObserver(() => {
+			const disconnect = onMutation();
+			if (disconnect) {
+				observer.disconnect();
+			}
+		});
+
+		observer.observe(document.body, {
+			childList: true,
+			subtree: true,
 		});
 	}
 
 	console.log("Automatic google auth connection script started.");
 
 	document.URL.startsWith("https://accounts.google.com/o/oauth2/auth") &&
-		getElement("[data-third-party-email]", true, (elements) =>
-			elements.find((element) => element?.textContent?.trim() === application),
+		getElements("[data-third-party-email]", (elements) =>
+			elements.some((element) => element.textContent?.trim() === application),
 		)
 			.then(() => getElement("[data-identifier]"))
 			.then((element) => setInterval(() => element.click(), 100));
 
 	document.URL.startsWith("https://accounts.google.com/signin/oauth/id") &&
-		getElement("#headingText", false, (element) =>
-			element?.textContent?.trim().includes(application),
+		getElement(
+			"#headingText",
+			(element) => element.textContent?.trim().includes(application) ?? false,
 		)
-			.then(() => getElement("button", true))
+			.then(() => getElements("button"))
 			.then((elements) => elements[1].click());
 
 	document.URL.startsWith("https://accounts.google.com/signin/oauth/consent") &&
 		getElement(
 			"#developer_info_glif",
-			false,
-			(element) => element?.textContent?.trim() === application,
+			(element) => element.textContent?.trim() === application,
 		)
 			.then(() => getElement("#submit_approve_access"))
 			.then((element) => element.click());
@@ -78,17 +117,16 @@
 	document.URL.startsWith("https://sdk.cloud.google.com/authcode.html") &&
 		getElement(
 			"code.auth-code",
-			false,
-			(element) => element?.textContent?.trim()?.length > 0,
+			(element) => (element.textContent?.trim().length ?? 0) > 0,
 		)
-			.then((element) => element?.textContent?.trim())
+			.then((element) => element.textContent?.trim())
 			.then((code) => GM_setClipboard(code, "text"))
-			.then(
-				() => GM_notification({
-					tag: 'google-sdk-autoconnect',
-					title: 'Google SDK Autoconnect',
-					text: 'Google SDK auth-code copied to clipboard.'
-				})
+			.then(() =>
+				GM_notification({
+					tag: "google-sdk-autoconnect",
+					title: "Google SDK Autoconnect",
+					text: "Google SDK auth-code copied to clipboard.",
+				}),
 			)
 			.then(window.close);
 })();
